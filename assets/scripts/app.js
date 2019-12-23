93c60c652290191fac351457cea4eb076c924d94
Aruna <myruala@gmail.com>
2019-12-23 20:01:27 +0200

    Adaptive up to 1920px for Technology page

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "919cd99239d91c0652e9";
+/******/ 	var hotCurrentHash = "f012a1820298645fe418";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
